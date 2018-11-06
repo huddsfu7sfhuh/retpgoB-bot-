c0932e276dbf90db91dde1f9a8397822d051040c
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-06 20:23:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,10 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
 client.on('ready', () => {
- console.log("welcome bot"); 
-console.log("log");
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`:(`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 });
 
-  client.user.setGame(D7i .,'https://www.twitch.tv/v5bz%27);
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
-});
 
-client.login("NTA5MzA0MDg3OTk3NzEwMzM5.DsMufQ.-fMKZ9Cs69f3jHjy6vnVtMMHvbk");
+client.login(process.env.BOT_TOKEN); 
