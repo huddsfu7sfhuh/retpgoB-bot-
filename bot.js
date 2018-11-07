7e8936355c3533f59dfa9cc5d1729ab5b708cd0c
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-07 19:58:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`👊 `,"http://twitch.tv/X_Xz")
-client.user.setStatus("dnd")
+client.user.setStatus("Online")
 });
 
 
